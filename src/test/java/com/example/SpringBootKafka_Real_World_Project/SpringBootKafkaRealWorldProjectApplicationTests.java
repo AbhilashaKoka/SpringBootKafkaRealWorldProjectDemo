53079a939d2c20ac9d6145338a9617b4b56937f2
AbhilashaKoka <kokaabhilasha@rediffmail.com>
2024-11-03 17:13:53 +0530

    commit1

---
+++ src/test/java/com/example/SpringBootKafka_Real_World_Project/SpringBootKafkaRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootKafka_Real_World_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaRealWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
